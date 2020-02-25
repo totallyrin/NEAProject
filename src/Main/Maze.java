a9e1a207dce6ca9d0f4a6a40b8ef856ac161dc3f
totallyrin <lucy.woloszczuk@gmail.com>
2020-02-25 11:42:13 +0000

    changed version to 0.5.6 fixed bugs

--- src/Main/Maze.java
+++ src/Main/Maze.java
@@ -44,58 +44,6 @@ public abstract class Maze extends JPanel implements Runnable {
         stop = false;
     }
 
-    // create default paint method
-    /*public void paintComponent(Graphics g, Mark[][] maze) {
-        super.paintComponent(g);
-        g.setColor(bg);
-        g.fillRect(20, 0, 10, 20); // start square
-        g.fillRect(mazeSize * 10 - 10, mazeSize * 10 + 10, 10, 10); // end square
-        maze[1][0] = Mark.PATH; // joins start square to maze
-        maze[mazeSize - 2][mazeSize - 1] = Mark.PATH; // joins end square to maze
-        for (int y = 0; y < mazeSize; y++) {
-            for (int x = 0; x < mazeSize; x++) {
-                switch (maze[x][y]) {
-                    case WALL: // if the cell is a wall, paint dark grey
-                        g.setColor(Color.DARK_GRAY);
-                        g.fillRect(x * 10 + 10, y * 10 + 10, 10, 10);
-                        break;
-                    case PATH: // if the cell is a path, paint white
-                        g.setColor(bg);
-                        g.fillRect(x * 10 + 10, y * 10 + 10, 10, 10);
-                        break;
-                    case SEARCH:
-                        g.setColor(yellow);
-                        g.fillRect(x * 10 + 10, y * 10 + 10, 10, 10);
-                        break;
-                    case END:
-                        if (!completedGen) {
-                            g.setColor(blue);
-                        } else {
-                            g.setColor(bg);
-                        }
-                        g.fillRect(x * 10 + 10, y * 10 + 10, 10, 10);
-                        break;
-                    case CURRENT: // if the cell is the current cell and maze is not completedGen, paint red, otherwise paint white
-                        if (!completedGen) {
-                            g.setColor(red);
-                        } else {
-                            g.setColor(bg);
-                        }
-                        g.fillRect(x * 10 + 10, y * 10 + 10, 10, 10);
-                        break;
-                    case ROUTE:
-                        g.setColor(Color.ORANGE);
-                        g.fillRect(x * 10 + 10, y * 10 + 10, 10, 10);
-                        break;
-                    case LINE:
-                        g.setColor(green);
-                        g.fillRect(x * 10 + 10, y * 10 + 10, 10, 10);
-                        break;
-                }
-            }
-        }
-    }*/
-
     // 'completes' the maze by setting dead ends and current cell to path
     Mark[][] complete(Mark[][] maze) {
         for (int x = 0; x < maze.length; x++) {
